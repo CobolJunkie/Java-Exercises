92dcf1ace6f3a7bb4c7781912b30646b35940250
CobolJunkie <107401086+CobolJunkie@users.noreply.github.com>
2022-06-28 16:14:48 +0200

    Finished Equal Sum Checker exercise

--- src/com/coboljunkie/MasterClassUdemy/DecimalComparator.java
+++ src/com/coboljunkie/MasterClassUdemy/DecimalComparator.java
@@ -1,21 +1,29 @@
 package com.coboljunkie.MasterClassUdemy;
 
-/*Write a method hasEqualSum with 3 parameters of type int.
+/*
+Write a method areEqualByThreeDecimalPlaces with two parameters of type double.
 
-        The method should return boolean and it needs to return true if the sum of the first and second parameter is equal to the third parameter. Otherwise, return false.
+        The method should return boolean and it needs to return true if two double numbers are the same up to three decimal places. Otherwise, return false.
 
 
         EXAMPLES OF INPUT/OUTPUT:
 
-        * hasEqualSum(1, 1, 1);  should return false since 1 + 1 is not equal to 1
+        * areEqualByThreeDecimalPlaces(-3.1756, -3.175); → should return true since numbers are equal up to 3 decimal places.
 
-        * hasEqualSum(1, 1, 2);  should return true since 1 + 1 is equal to 2
+        * areEqualByThreeDecimalPlaces(3.175, 3.176); → should return false since numbers are not equal up to 3 decimal places
 
-        * hasEqualSum(1, -1, 0);  should return true since 1 + (-1) is 1 - 1 and is equal to 0
+        * areEqualByThreeDecimalPlaces(3.0, 3.0); → should return true since numbers are equal up to 3 decimal places.
+
+        * areEqualByThreeDecimalPlaces(-3.123, 3.123); → should return false since numbers are not equal up to 3 decimal places.
 
 
-        NOTE: The hasEqualSum method  needs to be defined as public static like we have been doing so far in the course.
-        NOTE: Do not add a  main method to solution code.*/
+        TIP: Use paper and pencil.
+
+        TIP: Use casting.
+
+        NOTE: The areEqualByThreeDecimalPlaces method  needs to be defined as public static like we have been doing so far in the course.
+        NOTE: Do not add a  main method to solution code.
+*/
 
 public class DecimalComparator {
         public static boolean areEqualByThreeDecimalPlaces(double num1,double num2){
