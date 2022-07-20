fbe5c8d1beea17761f541270db8a977f7bbb2795
CobolJunkie <107401086+CobolJunkie@users.noreply.github.com>
2022-07-21 00:48:29 +0200

    Finished AreaCalculator

--- src/com/coboljunkie/MasterClassUdemy/AreaCalculator.java
+++ src/com/coboljunkie/MasterClassUdemy/AreaCalculator.java
@@ -37,4 +37,12 @@ Write a method named area with one double parameter named radius.
         NOTE: Do not add a main method to your solution code!*/
 
 public class AreaCalculator {
+    public static double area(double radius){
+        if (radius < 0) return -1.0;
+        else return radius * radius * Math.PI;
+    }
+    public static double area(double x, double y){
+        if (x < 0 || y < 0) return -1.0;
+        else return x * y;
+    }
 }
