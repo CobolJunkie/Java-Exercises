c95d8e48c741b0c427f03c7949c433c6a2a009fb
CobolJunkie <107401086+CobolJunkie@users.noreply.github.com>
2023-01-25 09:23:37 +0100

    added JavaDoc

--- src/com/coboljunkie/umc/section_5/exercise_12/PlayingCat.java
+++ src/com/coboljunkie/umc/section_5/exercise_12/PlayingCat.java
@@ -1,6 +1,17 @@
 package com.coboljunkie.umc.section_5.exercise_12;
-
+/** This class checks if a cat is playing based on the season and temperature
+ *
+ * @author Coboljunkie
+ * @author cj at coboljunkie.com
+ * @version 0.1
+ **/
 public class PlayingCat {
+    /** This method determines if a cat is playing based
+     *
+     * @param summer boolean to specify if its summer
+     * @param temperature the temperature in celsius
+     * @return true if the cat is playing
+     */
     public static boolean isCatPlaying(boolean summer, int temperature) {
         if (!summer && (temperature >= 25 && temperature <= 35)) {
             return true;
