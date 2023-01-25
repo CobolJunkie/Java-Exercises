cece6740154726bf2fe9dc120418b1547f6e3245
CobolJunkie <107401086+CobolJunkie@users.noreply.github.com>
2023-01-25 02:07:55 +0100

    added JavaDoc

--- src/com/coboljunkie/umc/section_5/exercise_10/MinutesToYearsDaysCalculator.java
+++ src/com/coboljunkie/umc/section_5/exercise_10/MinutesToYearsDaysCalculator.java
@@ -1,7 +1,17 @@
 package com.coboljunkie.umc.section_5.exercise_10;
-
+/** This class converts minutes to Years and Days
+ *
+ * @author Coboljunkie
+ * @author cj at coboljunkie.com
+ * @version 0.1
+ **/
 
 public class MinutesToYearsDaysCalculator {
+    /** this method converts a given value of minutes to years and days respectively and prints the output to console
+     *
+     * @param minutes the amount of minutes to convert
+     * @since 0.1
+     */
     public static void printYearsAndDays(long minutes){
         if (minutes < 0)System.out.println("Invalid Value");
         else {
--- src/com/coboljunkie/umc/section_5/exercise_11/IntEqualityPrinter.java
+++ src/com/coboljunkie/umc/section_5/exercise_11/IntEqualityPrinter.java
@@ -1,6 +1,17 @@
 package com.coboljunkie.umc.section_5.exercise_11;
-
+/** This class compares three int values
+ *
+ * @author Coboljunkie
+ * @author cj at coboljunkie.com
+ * @version 0.1
+ **/
 class IntEqualityPrinter {
+    /** This method compares three int values and outputs the result to the console
+     *
+     * @param a the first value to compare
+     * @param b the second value to compare
+     * @param c the thid value to compare
+     */
     public static void printEqual(int a, int b, int c){
         if (a < 0 || b < 0 || c < 0){
             System.out.println("Invalid Value");
