173651eb37b5ce16066a219fddbc19e0a673b3e8
CobolJunkie <107401086+CobolJunkie@users.noreply.github.com>
2023-01-26 22:56:22 +0100

    added JavaDoc

--- src/com/coboljunkie/umc/section_6/exercise_18/EvenDigitSum.java
+++ src/com/coboljunkie/umc/section_6/exercise_18/EvenDigitSum.java
@@ -1,6 +1,16 @@
 package com.coboljunkie.umc.section_6.exercise_18;
-
+/** This class contains a method to calculate sum of all even digits in a number
+ *
+ * @author Coboljunkie
+ * @author cj at coboljunkie.com
+ * @version 0.1
+ **/
 public class EvenDigitSum {
+    /**
+     * This method calculates the sum of all even digits in a number
+     * @param number the number to sum the even digits of
+     * @return the sum of all even digits
+     */
     public static int getEvenDigitSum(int number) {
         if (number < 0) return -1;
         int result = 0;
