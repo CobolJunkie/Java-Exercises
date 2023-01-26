5e51bb6ce31672bb09a8605c8a5cba10c8a2949d
CobolJunkie <107401086+CobolJunkie@users.noreply.github.com>
2023-01-26 22:52:37 +0100

    added JavaDoc

--- src/com/coboljunkie/umc/section_6/exercise_17/FirstLastDigitSum.java
+++ src/com/coboljunkie/umc/section_6/exercise_17/FirstLastDigitSum.java
@@ -1,7 +1,17 @@
 package com.coboljunkie.umc.section_6.exercise_17;
-
+/** This class contains a method to calculate the sum of the first and last digit of an int number
+ *
+ * @author Coboljunkie
+ * @author cj at coboljunkie.com
+ * @version 0.1
+ **/
 public class FirstLastDigitSum {
-
+    /** This method calculates the sum of the firs tand last digit of an int number
+     *
+     * @param number the number to take the first and last digit from
+     * @return the sum of the first and last digit
+     * @since 0.1
+     */
     public static int sumFirstAndLastDigit(int number) {
         if (number < 0) {
             return -1;
