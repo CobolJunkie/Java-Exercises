445a82df7fe6db77b5c42c97384afffdfb9e174e
CobolJunkie <107401086+CobolJunkie@users.noreply.github.com>
2023-01-28 23:17:27 +0100

    Added JavaDoc,refactored InputCalculator.java

--- src/com/coboljunkie/umc/section_6/exercise_28/InputCalculator.java
+++ src/com/coboljunkie/umc/section_6/exercise_28/InputCalculator.java
@@ -12,15 +12,14 @@ public class InputCalculator {
      */
     public static void inputThenPrintSumAndAverage(){
         Scanner scanner = new Scanner(System.in);
-        int sum = 0;
         int count = 0;
-
-        while(scanner.hasNextInt()){
+        int sum = 0;
+            while(scanner.hasNextInt())
+            {
             sum += scanner.nextInt();
-            count++;
+                count++;
             scanner.nextLine();
-
-        }
+            }
         System.out.println("SUM = " + sum + " AVG = " + Math.round((double) sum/count));
         scanner.close();
     }
---
+++ src/com/coboljunkie/umc/section_6/exercise_29/PaintJob.java
@@ -0,0 +1,4 @@
+package com.coboljunkie.umc.section_6.exercise_29;
+
+public class PaintJob {
+}
