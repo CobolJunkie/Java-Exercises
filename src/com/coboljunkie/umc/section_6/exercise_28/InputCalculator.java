040491de970365a29ad08165d748f2f48633b95c
CobolJunkie <107401086+CobolJunkie@users.noreply.github.com>
2023-01-28 22:29:34 +0100

    Added JavaDoc

--- src/com/coboljunkie/umc/section_6/exercise_28/InputCalculator.java
+++ src/com/coboljunkie/umc/section_6/exercise_28/InputCalculator.java
@@ -1,7 +1,15 @@
 package com.coboljunkie.umc.section_6.exercise_28;
 import java.util.Scanner;
-
+/** This class contains a method add numbers continuosly and output the sum after
+ *
+ * @author Coboljunkie
+ * @author cj at coboljunkie.com
+ * @version 0.1
+ **/
 public class InputCalculator {
+    /** this method adds numbers continuosly and outputs the sum after
+     *
+     */
     public static void inputThenPrintSumAndAverage(){
         Scanner scanner = new Scanner(System.in);
         int sum = 0;
